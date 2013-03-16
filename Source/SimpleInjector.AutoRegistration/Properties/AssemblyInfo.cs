4cd92f841b771a95c425c9345ec11fd232effedd
Keith Knight <keithaknight@gmail.com>
2013-03-16 10:59:40 -0700

    Minor versioning updates and nuget updates.

--- Source/SimpleInjector.AutoRegistration/Properties/AssemblyInfo.cs
+++ Source/SimpleInjector.AutoRegistration/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SimpleInjector.AutoRegistration")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An Auto-Registration extension for SimpleInjector")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SimpleInjector.AutoRegistration")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Keith A. Knight 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.*")]
+[assembly: AssemblyFileVersion("1.0.2.*")]
